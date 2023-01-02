295b95b0a4e941eb78700a2d98db7db1c562000c
Kelly Kilma de Sousa <kellykilmadesousa@gmail.com>
2023-01-02 17:35:44 -0300

    importacao de arquivos - funcoes auxiliares

---
+++ javascript/aula6 - importacao de arquivos/funcoes-auxiliares.js
@@ -0,0 +1,26 @@
+/*function gets() {
+    return 10;
+}
+
+function print(texto) {
+    console.log(texto);
+
+}
+
+module.exports = {gets, print}; */
+
+
+const entradas = [10, 5, 50, 10, 98, 23, 33, 44, 100, 1, 7];
+let i = 0;
+
+function gets() {
+    const valor = entradas[i];
+    i++;
+    return valor;
+}
+
+function print(texto) {
+    console.log(texto);
+}
+
+module.exports = { gets, print };
